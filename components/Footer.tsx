76d552377e9c0778a6111f1f245ffc6b06b25627
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2023-09-16 15:56:26 +1200

    Fixed footer for mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,32 +8,20 @@ const Footer = () => {
   return (
     <footer className='shadow-footerShadow w-full h-5 py-6 flex items-center justify-center gap-3'>
         <a href='https://github.com/Pranav-XP'>
-        <motion.span
-                 initial={{y:-50,opacity:0}}
-                 animate={{y:0,opacity:1}}
-                 transition={{duration:0.1}}>
-                    <VscGithubInverted size={20}></VscGithubInverted>
-                </motion.span>
+            <span>
+                <VscGithubInverted size={20}></VscGithubInverted>
+            </span>
         </a>
-
-<a href='https://www.linkedin.com/in/pranav-chand-16777318b/'>
-<motion.span 
-                initial={{y:-50,opacity:0}}
-                animate={{y:0,opacity:1}}
-                transition={{duration:0.2}}>
-                    <AiFillLinkedin size={30}></AiFillLinkedin>
-                </motion.span>
-</a>
-                
-              <a href="mailto:pranavchand777@gmail.com">
-              <motion.span
-                 initial={{x:50,opacity:0}}
-                 animate={{x:0,opacity:1}}
-                 transition={{duration:0.1}}>
-                    <MdEmail size={30}></MdEmail>
-                </motion.span>
-              </a>
-                
+        <a href='https://www.linkedin.com/in/pranav-chand-16777318b/'>
+          <span>
+            <AiFillLinkedin size={30}></AiFillLinkedin>
+          </span>
+        </a>
+        <a href="mailto:pranavchand777@gmail.com">
+          <span>
+            <MdEmail size={30}></MdEmail>
+          </span>
+        </a>
     </footer>
   )
 }
