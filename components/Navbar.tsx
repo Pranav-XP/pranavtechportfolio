5d0bf25f9181668cc54bfa14a105468615a82dec
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2023-09-18 00:29:31 +1200

    Refine styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import React from 'react'
-import { logo, profileImg } from '@/public/assets/images'
+import { logo} from '@/public/assets/images'
 import Link from "next/link";
 import { motion } from "framer-motion" 
 
 const Navbar = () => {
   return (
-    <div className='bg-navbarColor w-full shadow-navbarShadow h-20 sticky top-0 z-50  px-4'>
+    <div className='bg-navbarColor w-full shadow-navbarShadow h-18 sm:h-20 sticky top-0 z-50  px-4'>
         <div className=' max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between'>
             <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1}}>
                 <a href="#home"><Image className="w-12 hover:scale-110 duration-300" 
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Tools from "@/components/Tools";
 export default function Home() {
   return (
     <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight
-    overflow-x-hidden overflow-y-scroll scrollbar scrollbar-thumb-neutral-500 scrollbar-track-neutral-900">
+    overflow-x-hidden scrollbar scrollbar-thumb-neutral-500 scrollbar-track-neutral-900">
       <Navbar/>
       <div className="flex items-center justify-between gap-4">
       <aside className="hidden md:flex fixed h-full w-16 left-0 bottom-0">
